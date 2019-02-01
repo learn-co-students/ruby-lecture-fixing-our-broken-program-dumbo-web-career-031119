22aaed377fe1d480e2d00ea9a0cd9e3ea329f4b7
John Pena <jpena91@gmail.com>
2019-02-01 01:49:55 +0000

    Automatically backed up by Learn

--- spec/a_broken_program_spec.rb
+++ spec/a_broken_program_spec.rb
@@ -1,6 +1,6 @@
 describe 'A broken program' do
 
-  it 'does not raise an error when loaded' do
+  it 'does raise an error when loaded' do
     expect{
       load 'lib/a_working_program.rb'
     }.to raise_error
